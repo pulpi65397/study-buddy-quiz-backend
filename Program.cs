eb639680a674fc0cd554b397672426e61e2dce05
Piotr Oleksiuk <po83516@stud.uws.edu.pl>
2026-07-17 18:31:27 +0200

    Fix: disable config file watching to avoid inotify limit on Render free tier

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 using study_buddy_quiz.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.Sources
+    .OfType<Microsoft.Extensions.Configuration.FileConfigurationSource>()
+    .ToList()
+    .ForEach(source => source.ReloadOnChange = false);
 
 builder.Services.AddApplicationServices(builder.Configuration);
 
